d436d1a3b15af6f48db792348ea84891999180c9
Forrest Stonedahl <fsondahl@gmail.com>
2015-07-07 18:27:46 -0500

    fancier distance sensor test, with lighting and colors to say if the robot can see another robot nearby-ish (< 2.2 m)

--- Robot A/src/testdrive/DistanceSensorTest.java
+++ Robot A/src/testdrive/DistanceSensorTest.java
@@ -1,6 +1,7 @@
 package testdrive;
 
 import lejos.hardware.Button;
+import lejos.hardware.Sound;
 import lejos.hardware.lcd.LCD;
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3UltrasonicSensor;
@@ -10,23 +11,33 @@ import lejos.utility.Delay;
 public class DistanceSensorTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		EV3UltrasonicSensor sensor = new EV3UltrasonicSensor(SensorPort.S1);
 		sensor.enable();
 		
 		SampleProvider sp = sensor.getDistanceMode();
 		
 		float[] sample = new float[sp.sampleSize()];
-		sp.fetchSample(sample, 0);
-		LCD.clear();
-		LCD.drawString("Distance: " + sample[0], 0, 0);
-		Delay.msDelay(5000);
-		/*while(!Button.ENTER.isDown()){
+		float lastDistance = 0;
+
+		while(!Button.ENTER.isDown()){
+			LCD.clear();
 			sp.fetchSample(sample, 0);
-			distance = (int)sample[0];
-			LCD.drawString("IR: " + distance, 0, 0);
-			Delay.msDelay(3000);
-		}*/
+			float distance = sample[0];
+			LCD.drawString(String.format("Dist: %.2f", distance), 0, 4);
+			if ((lastDistance > 2.2 && distance <= 2.2)) {
+				//Sound.beep();
+				Sound.playNote(Sound.XYLOPHONE, 500, 100);
+			} else if ((lastDistance <= 2.2 && distance > 2.2)) {				
+				Sound.playNote(Sound.FLUTE, 700, 100);
+			}
+			lastDistance = distance;
+			if (distance > 2.2) {
+				Button.LEDPattern(1);				
+			} else {
+				Button.LEDPattern(2);
+			}
+			Delay.msDelay(100);
+		}
 		
 	}
 
